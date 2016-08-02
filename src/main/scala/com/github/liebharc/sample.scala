0f9f8485db7411836be37dc12058a8b059a85e2d
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-08-01 20:20:20 -0700

    Added some buttons with event handlers.

--- src/main/scala/com/github/liebharc/sample.scala
+++ src/main/scala/com/github/liebharc/sample.scala
@@ -1,15 +1,37 @@
 package com.github.liebharc
 
-import android.app.Activity
-import android.os.Bundle
+import android._
+import android.app._
+import android.os._
+import android.widget._
+import android.view._
 
 class MainActivity extends Activity with TypedFindView {
     lazy val textview = findView(TR.text)
+    lazy val buttonLow = findView(TR.buttonLow).asInstanceOf[Button]
+    lazy val buttonHigh = findView(TR.buttonHigh).asInstanceOf[Button]
+    lazy val buttonReplay = findView(TR.buttonReplay).asInstanceOf[Button]
 
     /** Called when the activity is first created. */
-    override def onCreate(savedInstanceState: Bundle): Unit = {
+    override def onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
         textview.setText("Hello world, from learnnotes")
     }
+
+  def tooLowSelected(view: View) {
+    textview.setText("Too low")
+  }
+
+  def perfectSelected(view: View) {
+    textview.setText("Perfect")
+  }
+
+  def tooHighSelected(view: View) {
+    textview.setText("Too High")
+  }
+
+  def replaySound(view: View) {
+    textview.setText("Replay")
+  }
 }
