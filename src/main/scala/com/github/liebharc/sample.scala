e4032b672d1705e3617733291c9b8f30dc0f250c
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-08-01 21:08:51 -0700

    We are able to play sounds!

--- src/main/scala/com/github/liebharc/sample.scala
+++ src/main/scala/com/github/liebharc/sample.scala
@@ -1,37 +1,63 @@
 package com.github.liebharc
 
+import java.util
+
 import android._
 import android.app._
 import android.os._
 import android.widget._
 import android.view._
+import android.media._
+
+case class SoundTriplet(tooLow: Int, perfect: Int, tooHigh: Int)
+
+case class Sounds(dString: Int)
 
 class MainActivity extends Activity with TypedFindView {
-    lazy val textview = findView(TR.text)
-    lazy val buttonLow = findView(TR.buttonLow).asInstanceOf[Button]
-    lazy val buttonHigh = findView(TR.buttonHigh).asInstanceOf[Button]
-    lazy val buttonReplay = findView(TR.buttonReplay).asInstanceOf[Button]
+  lazy val textView = findView(TR.text)
 
-    /** Called when the activity is first created. */
-    override def onCreate(savedInstanceState: Bundle) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
-        textview.setText("Hello world, from learnnotes")
-    }
+  /*
+    Useful links for soundpool:
+    https://dzone.com/articles/playing-sounds-android
+    http://stackoverflow.com/questions/17069955/play-sound-using-soundpool-example#17070454
+   */
+  lazy val soundPool = {
+    val attributes = new AudioAttributes.Builder()
+      .setUsage(AudioAttributes.USAGE_GAME)
+      .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+      .build()
+
+    new SoundPool.Builder()
+      .setAudioAttributes(attributes)
+      .build()
+  }
+
+  lazy val sounds = {
+    val dString = soundPool.load(this, R.raw.dstring, 1)
+    Sounds(dString)
+  }
+  /** Called when the activity is first created. */
+  override def onCreate(savedInstanceState: Bundle) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.main)
+      textView.setText("Hello world, from learnnotes")
+  }
 
   def tooLowSelected(view: View) {
-    textview.setText("Too low")
+    textView.setText("Too low")
   }
 
   def perfectSelected(view: View) {
-    textview.setText("Perfect")
+    textView.setText("Perfect")
   }
 
   def tooHighSelected(view: View) {
-    textview.setText("Too High")
+    textView.setText("Too High")
   }
 
   def replaySound(view: View) {
-    textview.setText("Replay")
+    val volume = 1.0f
+    soundPool.play(sounds.dString, volume, volume, 1, 0, 1f);
+    textView.setText("Replay")
   }
 }
