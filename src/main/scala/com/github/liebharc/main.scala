9d81c1c01dc1d00d1aff98555640888c048553c7
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-08-12 19:26:08 -0700

    Created a custom view pager which deactivates the swipes

--- src/main/scala/com/github/liebharc/main.scala
+++ src/main/scala/com/github/liebharc/main.scala
@@ -1,13 +1,18 @@
 package com.github.liebharc
 
+import android.content.Context
 import android.os._
-import android.support.v4.app.{Fragment, FragmentActivity, FragmentManager, FragmentPagerAdapter}
+import android.support.v4.app._
+import android.support.v4.view._
+import android.util.AttributeSet
+import android.view.MotionEvent
 
 class MainActivity
     extends FragmentActivity
-    with TypedFindView
+      with TypedFindView
     with QuizBehaviour
     with FeedbackBehaviour {
+
   /** Called when the activity is first created. */
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
@@ -29,5 +34,26 @@ class PagerAdapter(manager: FragmentManager) extends FragmentPagerAdapter(manage
       case 1 => new QuizFragment()
       case _ => null
     }
+  }
+}
+
+class CustomViewPager(context: Context, attr: AttributeSet) extends ViewPager(context, attr) {
+
+  var enabled: Boolean = false
+
+  override def onTouchEvent(ev: MotionEvent): Boolean = {
+    if (this.enabled) {
+      return super.onTouchEvent(ev)
+    }
+
+    return false
+  }
+
+  override def onInterceptTouchEvent(ev: MotionEvent): Boolean = {
+    if (this.enabled) {
+      return super.onInterceptTouchEvent(ev)
+    }
+
+    return false
   }
 }
